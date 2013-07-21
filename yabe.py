924938858ef742424fbfc3f8c6ed7a853fcb1786
Renne Rocha <renne@rennerocha.com>
2013-07-21 14:53:02 -0300

    If no posts, warn visitor.

--- yabe.py
+++ yabe.py
@@ -6,8 +6,14 @@ from models import Post
 @get('/')
 @view('post')
 def index():
-    post = Post.select().where(Post.published==True).order_by(Post.date).limit(1).get()
-    return {'post': post}
+    try:
+        last_post = Post.select().where(Post.published==True).order_by(Post.date).limit(1).get()
+        page_title = last_post.title
+    except Post.DoesNotExist:
+        last_post = None
+        page_title = 'Nothing posted yet.'
+        
+    return {'post': last_post, 'page_title': page_title}
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
